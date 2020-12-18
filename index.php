4dd3154b78f12051e63764a4fa1bdf3ae832c14d
Stephen Akuffo <sa.akuffo@gmail.com>
2020-12-17 22:59:02 -0500

    dynamic inv

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My Ansible Page</h1>
+
 <?php
-    echo "<h1>Hello World! This is my Ansible page.</h1>"
+echo "Hello World!";
 ?>
+
+</body>
+</html>
---
+++ playbooks/quick_start.sh
@@ -0,0 +1,7 @@
+ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/ping.yml
+sleep 3
+ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/yum-update.yml
+sleep 3
+ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/install-services.yml
+sleep 3
+ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/setup-app.yml

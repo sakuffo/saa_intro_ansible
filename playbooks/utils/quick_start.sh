a66163257c52c510b3ae007762004d081db5bf49
Stephen Akuffo <sa.akuffo@gmail.com>
2020-12-18 10:29:08 -0500

    ansi vars

--- playbooks/utils/quick_start.sh
+++
@@ -1,7 +0,0 @@
-ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/ping.yml
-sleep 3
-ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/yum-update.yml
-sleep 3
-ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/install-services.yml
-sleep 3
-ansible-playbook ~/tech_education/a_cloud_guru/intro_to_ansible/ansible/playbooks/setup-app.yml
